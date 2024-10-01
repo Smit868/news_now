a1135d386d3f540748e9fa7133e4a1139072bdd7
smit <ssatashiya484@rku.ac.in>
2024-10-01 09:32:21 +0530

    resolve bottomoverflow

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -20,96 +20,98 @@ class Land extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: EdgeInsets.all(padding),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(30.0),
-                child: Image.asset(
-                  'assets/images/indiagate.jpg',
-                  fit: BoxFit.cover,
-                  height: imageHeight,
-                  width: double.infinity,
-                ),
-              ),
-              SizedBox(height: screenHeight * 0.02),
-              Text(
-                'News from around the world for you',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: titleFontSize,
-                ),
-              ),
-              SizedBox(height: screenHeight * 0.02),
-              Text(
-                'Best time to read, take your time to read a little more of this world',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: subtitleFontSize,
-                  color: Colors.black54,
-                ),
-              ),
-              SizedBox(height: screenHeight * 0.02),
-              Row(
-                mainAxisAlignment:
-                    MainAxisAlignment.spaceEvenly, // Distribute buttons evenly
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => LoginPage(),
-                        ),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    ),
-                    child: Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: screenHeight * 0.02),
-                      child: Text(
-                        ' Login ',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: buttonFontSize,
-                        ),
-                      ),
-                    ),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(30.0),
+                  child: Image.asset(
+                    'assets/images/indiagate.jpg',
+                    fit: BoxFit.cover,
+                    height: imageHeight,
+                    width: double.infinity,
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SignUpPage(),
-                        ),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    ),
-                    child: Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: screenHeight * 0.02),
-                      child: Text(
-                        'SignUp',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: buttonFontSize,
-                        ),
-                      ),
-                    ),
+                ),
+                SizedBox(height: screenHeight * 0.02),
+                Text(
+                  'News from around the world for you',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: titleFontSize,
                   ),
-                ],
-              ),
-            ],
+                ),
+                SizedBox(height: screenHeight * 0.02),
+                Text(
+                  'Best time to read, take your time to read a little more of this world',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: subtitleFontSize,
+                    color: Colors.black54,
+                  ),
+                ),
+                SizedBox(height: screenHeight * 0.02),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment
+                      .spaceEvenly, // Distribute buttons evenly
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginPage(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: screenHeight * 0.02),
+                        child: Text(
+                          ' Login ',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: buttonFontSize,
+                          ),
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SignUpPage(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: screenHeight * 0.02),
+                        child: Text(
+                          'SignUp',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: buttonFontSize,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -28,39 +28,41 @@ class ProfilePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 20),
-            // Profile Picture
-            CircleAvatar(
-              child: Icon(Icons.person_off_outlined),
-              radius: 50,
-              backgroundColor: Colors.blue,
-            ),
-            SizedBox(height: 20),
-            // Username
-            buildTextFormField(Icons.person, "Username"),
-            // Phone Number
-            buildTextFormField(Icons.phone, "Phone Number"),
-            // Email
-            buildTextFormField(Icons.email, "E-mail"),
-            // Birthdate
-            buildTextFormField(Icons.calendar_today, "Birthdate"),
-            // Password
-            buildTextFormField(Icons.lock, "Password", isPassword: true),
-            SizedBox(height: 3),
-            // Edit Profile Button
-            ElevatedButton(
-              onPressed: () {
-                // Define button functionality
-              },
-              child: Text("Edit Profile"),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 20),
+              // Profile Picture
+              CircleAvatar(
+                child: Icon(Icons.person_off_outlined),
+                radius: 50,
+                backgroundColor: Colors.blue,
               ),
-            ),
-          ],
+              SizedBox(height: 20),
+              // Username
+              buildTextFormField(Icons.person, "Username"),
+              // Phone Number
+              buildTextFormField(Icons.phone, "Phone Number"),
+              // Email
+              buildTextFormField(Icons.email, "E-mail"),
+              // Birthdate
+              buildTextFormField(Icons.calendar_today, "Birthdate"),
+              // Password
+              buildTextFormField(Icons.lock, "Password", isPassword: true),
+              SizedBox(height: 5),
+              // Edit Profile Button
+              ElevatedButton(
+                onPressed: () {
+                  // Define button functionality
+                },
+                child: Text("Edit Profile"),
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
