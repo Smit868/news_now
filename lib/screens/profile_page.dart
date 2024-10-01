113544f9969acb6f45c837aaa595be1d89bf6428
smit <ssatashiya484@rku.ac.in>
2024-10-01 11:28:16 +0530

    added logout button

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:news_now/screens/home_page.dart';
-import 'package:icon/icon.dart';
+import 'package:news_now/screens/Login_page.dart';
+import 'package:news_now/screens/home_page.dart'; // Assuming you have this file
+import 'package:icon/icon.dart'; // Assuming you are using this package
 
 void main() {
   runApp(MaterialApp(
@@ -19,10 +20,11 @@ class ProfilePage extends StatelessWidget {
           icon: Icon(Icons.arrow_back),
           onPressed: () {
             Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => HomePage(),
-                ));
+              context,
+              MaterialPageRoute(
+                builder: (context) => HomePage(), // Navigate to HomePage
+              ),
+            );
           },
         ),
       ),
@@ -61,6 +63,24 @@ class ProfilePage extends StatelessWidget {
                   padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
                 ),
               ),
+              SizedBox(height: 20),
+              // Logout Button
+              ElevatedButton(
+                onPressed: () {
+                  // Navigate to the logout page
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LoginPage(), // Define LogoutPage
+                    ),
+                  );
+                },
+                child: Text("Logout"),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red, // Red color for Logout button
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                ),
+              ),
             ],
           ),
         ),
@@ -68,6 +88,7 @@ class ProfilePage extends StatelessWidget {
     );
   }
 
+  // Helper method to build TextFormFields
   Widget buildTextFormField(IconData icon, String label,
       {bool isPassword = false}) {
     return Padding(
